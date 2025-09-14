dc99fc94ab677cca04fa97dc33ed32f53720dfa0
ShayanMasoudzadeh <shayan.masoudzadeh81@gmail.com>
2025-09-15 00:43:19 +0330

    added quantity setter for slot data

--- src/slot_data.gd
+++ src/slot_data.gd
@@ -2,4 +2,11 @@ extends Resource
 class_name SlotData
 
 @export var item_data : ItemData
-@export var quantity : int
+@export var quantity : int : set = set_quantity
+
+func set_quantity(value: int) -> void:
+	quantity = value
+	if item_data:
+		if quantity > item_data.stack_size:
+			quantity = item_data.stack_size
+			push_error("%s can't exceed max stack size, setting quantity to stack size" % item_data.id)
