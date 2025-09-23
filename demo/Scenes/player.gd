25abb3ff4437e4091bc2156c5d1513e57b12ba54
ShayanMasoudzadeh <shayan.masoudzadeh81@gmail.com>
2025-09-23 13:45:07 +0330

    Added item pick up functionality to player inventory

--- demo/Scenes/player.gd
+++ demo/Scenes/player.gd
@@ -1,3 +1,16 @@
 extends Node
 
 @export var inventory_data : InventoryData
+
+func on_item_pickup(slot_data: SlotData) -> void:
+	var result : bool = inventory_data.add_to_inventory(slot_data)
+	if !result:
+		print("Item can't be picked up.")
+	#code for deleting the picked up item from world
+
+
+func _on_pick_wood_button_pressed() -> void:
+	on_item_pickup(SlotData.new(load("res://demo/items/wood.tres"), 1))
+
+func _on_pick_stone_button_pressed() -> void:
+	on_item_pickup(SlotData.new(load("res://demo/items/stone.tres"), 1))
