9a6ffd9fa54b5b064e69fdcb4683d3b0afa591e4
ShayanMasoudzadeh <shayan.masoudzadeh81@gmail.com>
2025-09-28 00:49:39 +0330

    debug item pickup

--- demo/Scenes/player.gd
+++ demo/Scenes/player.gd
@@ -3,9 +3,15 @@ extends Node
 @export var inventory_data : InventoryData
 
 func on_item_pickup(slot_data: SlotData) -> void:
-	var result : bool = inventory_data.add_to_inventory(slot_data)
-	if !result:
-		print("Item can't be picked up.")
+	for i in range(slot_data.quantity):
+		var single_item_slot = slot_data.get_duplicate()
+		single_item_slot.set_quantity(1)
+		var result : bool = inventory_data.add_to_inventory(single_item_slot)
+		if result:
+			slot_data.set_quantity(slot_data.quantity - 1)
+		else:
+			print("%s Item(s) can't be picked up." % slot_data.quantity)
+			break
 	#code for deleting the picked up item from world
 
 
